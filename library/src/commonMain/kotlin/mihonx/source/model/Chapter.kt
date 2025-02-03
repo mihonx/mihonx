54e2a14e64d459d624d03328f1eaf38a8e4473b6
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2025-02-03 20:48:45 +0600

    Add 'Chapter.language' and 'Chapter.type'

--- library/src/commonMain/kotlin/mihonx/source/model/Chapter.kt
+++ library/src/commonMain/kotlin/mihonx/source/model/Chapter.kt
@@ -4,11 +4,13 @@ package mihonx.source.model
  * Represents a chapter of a [Manga], containing metadata such as chapter number, volume, and more.
  *
  * @property key            Unique identifier for a chapter, scoped to a [Manga]
+ * @property language       Refer to [Source.language]. Should never be [Source.Language.MULTI]
  * @property number         The chapter number, with `-1` indicating an unknown number.
  * @property volume         The volume number, with `-1` indicating an unknown volume.
  * @property name           The optional name or title of the chapter.
  * @property dateUpload     The upload date of the chapter, represented as a Unix timestamp.
  * @property scanlators     List of scanlators (groups or individuals) who worked on this chapter.
+ * @property type           Type of the chapter (e.g. monochrome/colored or chapter/volume etc.)
  * @property lockStatus     Indicates the lock status of the chapter.
  * @property auxiliaryData  Some string containing auxiliary data related to the chapter.
  *
@@ -17,11 +19,13 @@ package mihonx.source.model
 @Suppress("MemberVisibilityCanBePrivate")
 class Chapter(
     val key: String,
+    val language: String,
     val number: Float = -1f,
     val volume: Float = -1f,
     val name: String? = null,
     val dateUpload: Long = 0,
     val scanlators: List<String> = emptyList(),
+    val type: String? = null,
     val lockStatus: ChapterLockStatus = ChapterLockStatus.NONE,
     val auxiliaryData: String = "",
 )
